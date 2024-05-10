de5a95b37b2046afe5cfd5af8f943abf1e4f52bf
mahdi <mahdimamashli1383@gmail.com>
2024-05-11 01:15:08 +0330

    create auth system logic

---
+++ src/auth-service.mjs
@@ -0,0 +1,25 @@
+import { user } from "../db.json";
+export const authService = {
+  login: async (username, password) => {
+    try {
+      const foundUser = user.find((u) => u.username === username);
+
+      if (!foundUser) {
+        return { message: "Invalid username or password.", success: false };
+      }
+      const isPasswordValid = password === foundUser.password;
+
+      if (isPasswordValid) {
+        return { message: "Login successful!", success: true };
+      } else {
+        return { message: "Invalid username or password.", success: false };
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      return {
+        message: "An unexpected error occurred. Please try again later.",
+        success: false,
+      };
+    }
+  },
+};
