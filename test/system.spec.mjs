80c3d82702a50ede2f3a6b038bd9ea3925663299
mahdi <mahdimamashli1383@gmail.com>
2024-05-11 10:14:11 +0330

    "Update auth system test to use User class and deep equal"

--- test/system.spec.mjs
+++ test/system.spec.mjs
@@ -2,17 +2,15 @@ import { expect } from "chai";
 import { authService } from "../src/auth-service.mjs";
 import { ConnectionDatabase } from "../src/db.config.mjs";
 import { User } from "../src/user.model.mjs";
+
 describe("system test", () => {
   test("Login with valid credentials (system test)", async () => {
     let server = new ConnectionDatabase();
 
-    const user = new User("user1", "password1").create();
+    const user = new User("user1", "password1");
 
-    const isLoggedIn = await authService.login(
-      user["username"],
-      user["password"]
-    );
-    expect(isLoggedIn).to.equal({
+    const isLoggedIn = await authService.login(user.username, user.password);
+    expect(isLoggedIn).to.deep.equal({
       success: true,
       message: "Login successful!",
     });
@@ -24,7 +22,7 @@ describe("system test", () => {
     let server = new ConnectionDatabase();
 
     const isLoggedIn = await authService.login("invalid_user", "password1");
-    expect(isLoggedIn).to.be.equal({
+    expect(isLoggedIn).to.deep.equal({
       message: "Invalid username or password.",
       success: false,
     });
@@ -36,7 +34,7 @@ describe("system test", () => {
     let server = new ConnectionDatabase();
 
     const isLoggedIn = await authService.login("user1", "invalid_password");
-    expect(isLoggedIn).to.be.equal({
+    expect(isLoggedIn).to.deep.equal({
       message: "Invalid username or password.",
       success: false,
     });
